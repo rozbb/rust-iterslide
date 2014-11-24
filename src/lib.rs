9263f3130ec93e8b93da4e3427f94a27bba21857
Santiago Lapresta <santiago.lapresta@gmail.com>
2014-11-24 04:16:38 +0100

    Implement Slide iterator

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,84 @@
+#![feature(macro_rules)]
+
+pub struct Slide<T: Iterator<A>, A> {
+  iter: T,
+  n: uint,
+  window: Vec<A>
+}
+
+macro_rules! return_if(
+  ($cond:expr, $value:expr) => (
+    if $cond {
+      return $value;
+    }
+  );
+)
+
+impl<A: Clone, T: Iterator<A>> Slide<T, A> {
+  fn push_window(&mut self) -> bool {
+    let iter_next = self.iter.next();
+
+    if iter_next.is_some() {
+      self.window.push(iter_next.unwrap());
+      true
+    } else {
+      false
+    }
+  }
+
+  fn new(iter: T, n: uint) -> Slide<T, A> {
+    Slide{
+      iter: iter,
+      n: n,
+      window: vec![]
+    }
+  }
+}
+
+impl<A: Clone, T: Iterator<A>> Iterator<Vec<A>> for Slide<T, A> {
+  fn next(&mut self) -> Option<Vec<A>> {
+    if self.n == 0 {
+      return None
+    }
+
+    return_if!(!self.push_window(), None);
+
+    loop {
+      let window_status = self.window.len().cmp(&self.n);
+      
+      match window_status {
+        Greater => { self.window.remove(0); }
+        Equal => { return Some(self.window.clone()); }
+        Less => { return_if!(!self.push_window(), None); }
+      }
+    }
+  }
+}
+
 #[test]
-fn it_works() {
+fn test_slide() {
+  let mut slide_iter = Slide::new(vec![1i, 2, 3, 4, 5].into_iter(), 3);
+  assert_eq!(slide_iter.next().unwrap(), vec![1, 2, 3])
+  assert_eq!(slide_iter.next().unwrap(), vec![2, 3, 4])
+  assert_eq!(slide_iter.next().unwrap(), vec![3, 4, 5])
+  assert!(slide_iter.next().is_none())
+}
+
+#[test]
+fn test_slide_equal_window() {
+  let mut slide_iter = Slide::new(vec![1i, 2, 3, 4, 5].into_iter(), 5);
+  assert_eq!(slide_iter.next().unwrap(), vec![1, 2, 3, 4, 5])
+  assert!(slide_iter.next().is_none())
+}
+
+#[test]
+fn test_slide_zero_window() {
+  let mut slide_iter = Slide::new(vec![1i, 2, 3, 4, 5].into_iter(), 0);
+  assert!(slide_iter.next().is_none())
+}
+
+#[test]
+fn test_slide_overlong_window() {
+  let mut slide_iter = Slide::new(vec![1i, 2, 3, 4, 5].into_iter(), 7);
+  assert!(slide_iter.next().is_none())
 }
